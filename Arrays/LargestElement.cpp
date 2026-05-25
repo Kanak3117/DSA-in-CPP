4ba823b5eee96544e2f16d7ee87bbb6f8796ed9e
Kanak Varshney <kanak3117@gmail.com>
2026-05-25 18:26:35 +0530

    Update LargestElement.cpp

--- Arrays/LargestElement.cpp
+++ Arrays/LargestElement.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+        int maxi = nums[0];
+        for(int i =0; i< nums.size(); i++){
+            if(nums[i] > maxi) maxi = nums[i];
+        }
+        return maxi;
+    }
+};
