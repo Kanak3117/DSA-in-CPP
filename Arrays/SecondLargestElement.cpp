3acbf7d5f7d7e8a2ffa82290300e9411cbd29c73
Kanak Varshney <kanak3117@gmail.com>
2026-05-25 18:27:54 +0530

    Update SecondLargestElement.cpp

--- Arrays/SecondLargestElement.cpp
+++ Arrays/SecondLargestElement.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int secondLargestElement(vector<int>& nums) {
+        if(nums.size() <2) return -1;
+        int large = INT_MIN;      
+        int secondl = INT_MIN;
+        for(int i =0; i <nums.size(); i++){
+            if(nums[i] > large){
+                secondl = large;
+                large = nums[i];
+            }
+            else if(nums[i] > secondl && nums[i] != large) secondl = nums[i];
+        }
+        return (secondl == INT_MIN) ? -1 : secondl;
+    }
+};
